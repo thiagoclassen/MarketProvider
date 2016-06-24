fc584bf1e9b5e78bb63393311a336f7d7c05e495
Thiago Classen <tgclassen.dev@gmail.com>
2016-06-24 17:08:10 -0300

    updating

--- src/market/data/FileManager.java
+++ src/market/data/FileManager.java
@@ -1,48 +1,67 @@
 package market.data;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 
 /**
  * @author Thiago
  *
  */
 public class FileManager {
-	
-	private String fileName;
-	private String line;
-	private BufferedReader bufferedReader; 
-	
-	public FileManager(String fileName){
-		this.fileName = fileName;
-		try {
-			this.bufferedReader = new BufferedReader(new FileReader(this.fileName));
-		} catch (FileNotFoundException e) {
-			System.out.println("Unable to open file '" + this.fileName + "'");
-		}
-	}
-	
-	public void printFile(){
-		try {
-			while ((this.line = this.bufferedReader.readLine()) != null) {
-				System.out.println(line);
-			}
-		} catch (IOException e) {
-			System.out.println("Error reading file '" + this.fileName + "'");
-		}
-	}
-	
-	public void closeBuffer(){
-		try {
-			this.bufferedReader.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
+	public void teste() {
+		JSONParser parser = new JSONParser();
+		// String s = "[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
+		try {
+			Object obj = parser.parse(new FileReader("items.JSON"));
 
+			JSONArray array = (JSONArray) obj;
+
+			System.out.println("The 2nd element of array");
+			System.out.println(array.get(1));
+			System.out.println();
+
+		} catch (org.json.simple.parser.ParseException | IOException pe) {
+
+			System.out.println("position: " + pe);
+			System.out.println(pe);
+		}
+	}
 }
+
+// private String fileName;
+// private String line;
+// private BufferedReader bufferedReader;
+//
+// public FileManager(String fileName){
+// this.fileName = fileName;
+// try {
+// this.bufferedReader = new BufferedReader(new FileReader(this.fileName));
+// } catch (FileNotFoundException e) {
+// System.out.println("Unable to open file '" + this.fileName + "'");
+// }
+// }
+//
+// public void printFile(){
+// try {
+// while ((this.line = this.bufferedReader.readLine()) != null) {
+// System.out.println(line);
+// }
+// } catch (IOException e) {
+// System.out.println("Error reading file '" + this.fileName + "'");
+// }
+// }
+//
+// public void closeBuffer(){
+// try {
+// this.bufferedReader.close();
+// } catch (IOException e) {
+// // TODO Auto-generated catch block
+// e.printStackTrace();
+// }
+// }
+
+// }
--- src/market/main/MarketProvider.java
+++ src/market/main/MarketProvider.java
@@ -1,23 +1,45 @@
 package market.main;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 
-import market.client.Adress;
-import market.client.Client;
-import market.item.Item;
-import market.order.OrderItem;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class MarketProvider {
 	
 	public static void main(String[] args){
 		
-		Client teste = new Client(new Adress("Rua teste", 321, 84645431, "Bairro Teste", "Curitiba"), "Cliente A", "321654");
+		JSONParser parser = new JSONParser();
+		// String s = "[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
+		try {
+			Object obj = parser.parse(new FileReader("data/items.JSON"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+			
+			JSONArray list = (JSONArray)jsonObject.get("items");
+			
+			 Iterator<JSONObject> iterator = list.iterator();
+	            while (iterator.hasNext()) {
+	                System.out.println(iterator.next().get("telefone"));
+	            }
+			System.out.println();
+			
+			
+		} catch (org.json.simple.parser.ParseException | IOException pe) {
+
+			System.out.println("position: " + pe);
+			System.out.println(pe);
+		}
 		
-		OrderItem orderItem = new OrderItem(new Item("prdNome", "prdTipo", BigDecimal.valueOf(1.20f)), 1);
-		
-		ArrayList<OrderItem> prods = new ArrayList<OrderItem>();
-		prods.add(orderItem);
+//		Client teste = new Client(new Adress("Rua teste", 321, 84645431, "Bairro Teste", "Curitiba"), "Cliente A", "321654");
+//		
+//		OrderItem orderItem = new OrderItem(new Item("prdNome", "prdTipo", BigDecimal.valueOf(1.20f)), 1);
+//		
+//		ArrayList<OrderItem> prods = new ArrayList<OrderItem>();
+//		prods.add(orderItem);
 		
 //		NewOrder order = new NewOrder(prods, Calendar.getInstance(), teste, new Veiculo());
 		
